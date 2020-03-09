00946bffa25f31228d79931d4d9affd446beeadf
Hygor Christian Dias <37549730+hygorchristian@users.noreply.github.com>
2020-03-09 11:07:39 -0300

    (feat:code) changes state for the code screen

--- src/screens/Code/Code.jsx
+++ src/screens/Code/Code.jsx
@@ -10,7 +10,7 @@ import bg from '~/assets/img/qrcode.png'
 function Code() {
   const dispatch = useDispatch()
 
-  const [code, setCode] = useState('PARTIC_EVENTO_BSB_2020_2')
+  const [code, setCode] = useState(null)
   const { message, error, loading } = useSelector(state => state.code)
 
   const validateCode = () => {
@@ -53,62 +53,3 @@ function Code() {
 }
 
 export default Code;
-
-
-
-
-/*<Container>*/
-/*  <Text>Code</Text>*/
-/*  <Input*/
-/*    placeholder="Type your code here"*/
-/*    value={code}*/
-/*    onChangeText={value => setCode(value)}*/
-/*  />*/
-/*  <Button onPress={validateCode}>Register</Button>*/
-/*  {loading && <Text>Loading...</Text>}*/
-/*</Container>*/
-
-
-
-// import React, { useState, useEffect } from 'react';
-
-// import { Container, Text, Input, Image, Content, Button, Welcome } from './styles';
-// import { AuthActions } from "~/store/ducks/auth";
-// import { useDispatch, useSelector } from "react-redux";
-// import { showErrorMessage } from "~/utils/snackbar";
-// import { useNavigation } from "react-navigation-hooks";
-//
-// import bg from '~/assets/img/bg.png'
-//
-// function Login() {
-//   const dispatch = useDispatch()
-//   const navigation = useNavigation()
-//
-//   const [username, setUsername] = useState(null)
-//   const { isAuth, error } = useSelector(state => state.auth)
-//
-//   const handleLogin = () => {
-//     if(!username || username.length === 0){
-//       showErrorMessage('You must provide a username')
-//     }else{
-//       dispatch(AuthActions.loadAuthRequest(username))
-//     }
-//   }
-//
-//   useEffect(() => {
-//     if(error){
-//       showErrorMessage(error)
-//     }
-//   }, [error])
-//
-//   if(isAuth){
-//     navigation.navigate('HomeBottom')
-//   }
-//
-//   return (
-
-//   )
-// }
-//
-// export default Login;
-
